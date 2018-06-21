a1a2acc8a11bb2ae716a1a5396546d9b31c93691
John Apostolakis <john.apostolakis@cern.ch>
2018-06-21 18:17:40 +0200

    (Unfinished) Brought general proxy (JoiningProxyVecRNG) up to level of MRG32k3a
    Problem remains in compilation with the type of 'scalar' RNG state - from traits
    Compilation error remains when using it due to signature of GetStateAt / SetStateAt

--- include/VecMath/Rng/JoiningProxyVecRNG.h
+++ include/VecMath/Rng/JoiningProxyVecRNG.h
@@ -11,29 +11,32 @@ template <typename BaseVecRNGType,
           typename BaseScalarRNGType,
           typename BackendT>
    class JoiningProxyVecRNG :
-     // public BaseVecRNGType /* <BackendT> */    // Type 1: derive & extend
-     public VecRNG< JoiningProxyVecRNG<BaseVecRNGType, BaseScalarRNGType, BackendT> >  // Type 2: Use 'has'
+     public BaseVecRNGType /* <BackendT> */    // Type 1: derive & extend
+     // public VecRNG< JoiningProxyVecRNG<BaseVecRNGType, BaseScalarRNGType, BackendT> >  // Type 2: Use 'has'
 {
 public:
    // using State_t= typename RNG_traits<BaseVecRNGType> <BaseVecRNGType,BaseScalarRNGType,BackendT>> ::State_t;
-   using BaseRNGState_scalar= typename RNG_traits<BaseVecRNGType> /*<ScalarBackend>>*/ ::State_t;
-   using BaseRNGState_vector= typename RNG_traits<BaseScalarRNGType> /*<VectorBackend>>*/ ::State_t;
+   using BaseRNGState_scalar= typename RNG_traits<BaseScalarRNGType> /*<ScalarBackend>>*/ ::State_t;
+   using BaseRNGState_vector= typename RNG_traits<BaseVecRNGType> /*<VectorBackend>>*/ ::State_t;
    using State_t= BaseRNGState_vector;
+
+   /** @brief Constructor: 'create' empty instance - not associated with set of scalar states */
+   JoiningProxyVecRNG();
    
-   /** @brief Constructor: 'create' vector instance from a set of sequential states */
+   /** @brief Constructor: 'create' vector instance from a set of scalar states */
    JoiningProxyVecRNG( BaseScalarRNGType* trackPrng[], int numStates );
    // JoiningProxyVecRNG( BaseRNGState_scalar* trackPrng[], int vecLength );
 
    /** @brief Split apart this class - ensure that the state of its parts is fed back */
-   //          - i.e. the state gets copied back to each 'track' sequential RNG state
+   //          - i.e. the state gets copied back to each 'track' scalar RNG state
    ~JoiningProxyVecRNG();
-   
-   /** @brief Bare constructor - only for use with 'join' and 'split' methods */
-   // JoiningProxyVecRng();
 
+   /** @brief Create 'vector' state by copying the array of per-track states */
    void Join( BaseScalarRNGType* trackRngState[], int numStates );
    // void Join( BaseScalarRNGState_scalar* trackRngState[], int numStates );   
    // Join( SequentialRNG*       trackPrng[VecLen],     int /*numVariatesExpected*/ );
+
+   /** @brief De-couple 'vector' state - copy back the per-track states into original arrays */
    void Split();
 
 /*****   
@@ -52,16 +55,15 @@ public:
   void Initialize(State_t *states, unsigned int nthreads)
   { static_cast<DerivedT *>(this)->template Initialize(states, nthreads); }
  *****/
-   
+/*****   
   // ------------------------------------------------------------------------------------    
   // Expected methods for VecRNG ... needed in 'has' design
   // 
-
   template <typename ReturnBackendType>
   VECCORE_ATT_HOST_DEVICE
   VECCORE_FORCE_INLINE
      typename ReturnBackendType::Double_v Kernel(State_t& state);
-
+*****/
 /*********************  
   // ------------------------------------------------------------------------------------  
   // Methods provided by VecRNG ... no need in 'has' design 
@@ -88,15 +90,24 @@ public:
   { return min+(max-min)*static_cast<DerivedT *>(this)-> template Uniform<BackendT>(state); }
  *******************/
 
+   VECCORE_ATT_HOST_DEVICE   
+      typename BackendT::Double_v Uniform() ; // { return this->Uniform<VectorBackend>(); }
+
+   void PrintComponents() const;
+   
+protected:
+   static typename BackendT::Double_v  dbVec;
+   static constexpr int gVectorSize= VectorSize<decltype(dbVec)>();
+  
 private:
    // Location of track's RNG objects - in order to copy state back
-   // BaseRNGState_scalar* fSequentialTrackPrng[BackendT::VectorSize];
-   BaseScalarRNGType*  fSequentialTrackPrng[BackendT::VectorSize];
-   
+   BaseScalarRNGType*  fScalarTrackPrng[gVectorSize];
+   bool                fFullState= false;
    // BaseRNGState_vector  fVectorRngState;
    // BaseVecRNGType       fVectorRng;  //  Vector RNG which has 'sucked up' scalar state
 };
 
+/***
 template <typename BaseVecRNGType,
           typename BaseScalarRNGType,
           typename BackendT>
@@ -106,8 +117,49 @@ struct RNG_traits<JoiningProxyVecRNG<BaseVecRNGType, BaseScalarRNGType, BackendT
   struct StateJoined { BaseVecRNGType fVectorRng; }
   using State_t = StateJoined;
 };
+***/
 
-// Implementation
+
+//  ---------------------------================---------------------------
+template <typename BaseVecRNGType,
+          typename BaseScalarRNGType,   
+          typename BackendT>
+   JoiningProxyVecRNG<BaseVecRNGType, BaseScalarRNGType, BackendT>::JoiningProxyVecRNG()
+{
+   // fScalarTrackPrng= new BaseScalarRNGType* [gVectorSize];
+   
+   fFullState= false;  // Not
+   BaseVecRNGType::Initialize();   
+   for( int i= 0; i < gVectorSize; ++i )
+      fScalarTrackPrng[i] = nullptr;   
+}
+
+//  ---------------------------================---------------------------
+template <typename BaseVecRNGType,
+          typename BaseScalarRNGType,   
+          typename BackendT>
+   JoiningProxyVecRNG<BaseVecRNGType, BaseScalarRNGType, BackendT>::JoiningProxyVecRNG( BaseScalarRNGType* trackPrng[], int numStates )
+   : JoiningProxyVecRNG<BaseVecRNGType, BaseScalarRNGType, BackendT>()
+{
+   BaseVecRNGType::Initialize();
+   fFullState= false;
+   
+   // fScalarTrackPrng= new BaseScalarRNGType* [gVectorSize];   
+   Join( trackPrng, numStates );
+}
+
+//  ---------------------------================---------------------------
+template <typename BaseVecRNGType,
+          typename BaseScalarRNGType,   
+          typename BackendT>
+   JoiningProxyVecRNG<BaseVecRNGType, BaseScalarRNGType, BackendT>::~JoiningProxyVecRNG()
+{
+   if( fFullState ) Split();
+   // delete[] fScalarTrackPrng;
+}
+
+//  ---------------------------================---------------------------
+// Implementation of other methods
 template <typename BaseVecRNGType,
           typename BaseScalarRNGType,   
           typename BackendT>
@@ -118,26 +170,18 @@ void
    assert( numStates == VectorSize<BackendT> ); //  BackendT::VectorSize );
    
    // Keep record of locations of per-track PRNG - in order to copy back the final state
-   for( int i= 0; i < BackendT::VectorSize; ++i )
-      fSequentialTrackPrng[i] = trackPrng[i];
+   for( int i= 0; i < gVectorSize; ++i )
+      fScalarTrackPrng[i] = trackPrng[i];
 
-   VecRng<BackendT>::Initialize(); // fSequentialTrackPrng, BackendT::VectorSize );
+   BaseVecRNGType::Initialize(); // fScalarTrackPrng, BackendT::VectorSize );
 
-   // NEED to copy ‘in’ the states of the RNGs 
-   // Ideas for potential methods:
-   // 1.) Copy the state of the array of 
-   // CopyState( fSequentialTrackPrng );
-
-   //  Use an extension of the existing (VecRNG) methods
-   //   SetState(State_t *state) { fState = state; }
-   // e.g.
-   //   SetState(State_s *state, int lane);
-
-   // and the existing
-   //  State_t* GetState() const { return fState; }
-
-   for( int i= 0; i < BackendT::VectorSize; ++i )
-      SetState( fSequentialTrackPrng[i].GetState(), i);
+   // Copy ‘in’ the states of the RNGs 
+   for( int i= 0; i < gVectorSize; ++i ) {
+      assert( trackPrng[i] != nullptr );      
+      BaseVecRNGType::SetStateAt( i, fScalarTrackPrng[i]->GetState() );
+   }
+   
+   fFullState= true;   
 }
 
 //  ---------------------------================---------------------------
@@ -147,23 +191,27 @@ template <typename BaseVecRNGType,
           typename BackendT>
 void JoiningProxyVecRNG<BaseVecRNGType, BaseScalarRNGType, BackendT>::Split()
 {
-   for( int i= 0; i < BackendT::VectorSize; ++i )   
-      fSequentialTrackPrng[i].SetState( VecRNG<BackendT>::GetState().fVectorRNG.GetPartOfState(i) ); // Sets full state
-                                        
+   for( int i= 0; i < gVectorSize; ++i ) {
+      fScalarTrackPrng[i]->SetState( VecRNG<BackendT>::GetStateAt(i) );
+      fScalarTrackPrng[i] = nullptr;
+   }
+   fFullState= false;
 }
 
 //  ---------------------------================---------------------------
 
+/***/
+VECCORE_ATT_HOST_DEVICE
 template <typename BaseVecRNGType,
           typename BaseScalarRNGType,   
           typename BackendT>
-template <class ReturnTypeBackendT>   
-typename ReturnTypeBackendT::Double_v
-  JoiningProxyVecRNG<BaseVecRNGType, BaseScalarRNGType, BackendT>::
-   ::Kernel(State_t& state)
+  typename BackendT::Double_v
+    JoiningProxyVecRNG<BaseVecRNGType, BaseScalarRNGType, BackendT>::Uniform()
 {
-   BaseVecRNGType::Kernel( tstate.fBaseState );
+   assert( fFullState );
+   return this->BaseVecRNGType::template Uniform<BackendT>();
 }
+/****/
 
 }
 }
